82b1d05a00121e7209e878eace09e3827de6747a
GlintonLiao <815190191@qq.com>
2021-03-21 15:17:47 +0800

    Completed first oart of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Guotong Liao
  */
 public class LeapYear {
 
@@ -11,6 +11,16 @@ public class LeapYear {
             System.out.printf("%d is a leap year.\n", year);
         } else {
             System.out.printf("%d is not a leap year.\n", year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 
