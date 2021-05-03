ead65e55dfbe95ca37bc335b94b2962a82cc6be1
GlintonLiao <815190191@qq.com>
2021-05-03 17:58:07 +0800

    Update hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,54 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    int[][] grid;
+    WeightedQuickUnionUF UnionFind;
+    int openSize = 0;
 
+
+    //Create N-by-N grid with all sites initially blocked
+    public Percolation(int N) {
+        grid = new int[N][N];
+        UnionFind = new WeightedQuickUnionUF(N * N - 1);
+
+        // Virtual head node
+        for (int i = 0; i < N; i += 1) {
+            UnionFind.union(-1, i);
+        }
+
+        // Virtual tail node
+        for (int j = N * N - N; j < N * N; j += 1) {
+            UnionFind.union(j, N * N + 1);
+        }
+    }
+
+    // 1 -> Open; 0 -> Block
+    public void open(int row, int col) {
+        grid[row][col] = 1;
+        openSize += 1;
+    }
+
+    public boolean isOpen(int row, int col) {
+        return grid[row][col] == 1;
+    }
+
+    // Connected to the virtual head node -> full
+    public boolean isFull(int row, int col) {
+        int boxIndex = xyTo1D(row, col);
+        return UnionFind.connected(-1, boxIndex);
+    }
+    
+    public int numberOfOpenSites() {
+        return openSize;
+    }
+
+    // if the virtual head node and the virtual tail node is connected, return true;
+    public boolean percolates() {
+        return UnionFind.connected(-1, grid.length * grid.length - 1);
+    }
+
+    // helper method to get the index of the box
+    private int xyTo1D(int row, int col) {
+        return col * row - 1;
+    }
 }
