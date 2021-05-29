3c330a0ccb1571caf14a06a94bf85a519d38f681
GlintonLiao <815190191@qq.com>
2021-05-29 20:09:52 +0800

    Completed hw4

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,6 +1,144 @@
 package hw4.puzzle;
 
-public class Board {
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Board implements WorldState {
+    private int size;
+    private int BLANK = 0;
+    private int[][] board;
+
+    public Board(int[][] tiles) {
+        size = tiles.length;
+        board = new int[size][size];
+
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                board[i][j] = tiles[i][j];
+            }
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int tileAt(int i, int j) {
+        if (i < 0 || i > size() - 1 || j < 0 || j > size() - 1) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            return board[i][j];
+        }
+    }
+
+    @Override
+    public Iterable<WorldState> neighbors() {
+        Queue<WorldState> neighbors = new Queue<>();
+        int hug = size();
+        int bug = -1;
+        int zug = -1;
+        for (int rug = 0; rug < hug; rug++) {
+            for (int tug = 0; tug < hug; tug++) {
+                if (tileAt(rug, tug) == BLANK) {
+                    bug = rug;
+                    zug = tug;
+                }
+            }
+        }
+        int[][] ili1li1 = new int[hug][hug];
+        for (int pug = 0; pug < hug; pug++) {
+            for (int yug = 0; yug < hug; yug++) {
+                ili1li1[pug][yug] = tileAt(pug, yug);
+            }
+        }
+        for (int l11il = 0; l11il < hug; l11il++) {
+            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
+                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
+                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
+                    ili1li1[l11il][lil1il1] = BLANK;
+                    Board neighbor = new Board(ili1li1);
+                    neighbors.enqueue(neighbor);
+                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
+                    ili1li1[bug][zug] = BLANK;
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    public int hamming() {
+        int hammingDis = 0;
+
+        for (int i = 0; i < size(); i += 1) {
+            for (int j = 0; j < size(); j += 1) {
+                int goal = i * size() + j + 1;
+                if (goal < size() * size() && goal != board[i][j]) {
+                    hammingDis += 1;
+                }
+            }
+        }
+
+        return hammingDis;
+    }
+
+    public int manhattan() {
+        int manhattandDis = 0;
+
+        for (int i = 0; i < size(); i += 1) {
+            for (int j = 0; j < size(); j += 1) {
+                if (board[i][j] == 0) {
+                    continue;
+                }
+
+                int ii = (board[i][j] - 1) / size();
+                int jj = (board[i][j] - 1) % size();
+                manhattandDis +=  Math.abs(i - ii) + Math.abs(j - jj);
+            }
+        }
+
+        return manhattandDis;
+    }
+
+    @Override
+    public int estimatedDistanceToGoal() {
+        return manhattan();
+    }
+
+    public boolean equals(Object y) {
+        if (this == y) {
+            return true;
+        }
+        if (y == null) {
+            return false;
+        }
+        if (this.getClass() != y.getClass()) {
+            return false;
+        }
+
+        Board yy = (Board) y;
+        if (yy.size() != this.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size(); i += 1) {
+            for (int j = 0; j < size(); j += 1) {
+                if (board[i][j] != yy.board[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(size, BLANK);
+        result = 31 * result + Arrays.hashCode(board);
+        return result;
+    }
 
     /** Returns the string representation of the board. 
       * Uncomment this method. */
